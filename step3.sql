34fe569776b51c009bfce176c25f400bc2c07545
github20161003464 <823508091@qq.com>
2017-05-13 13:18:45 +0800

    Update step3.sql

--- step3.sql
+++ step3.sql
@@ -1,7 +1,7 @@
 #3-1.把peoples表中school不是GDUFS的人全部找出来？（包括school为NULL的人）写出MySQL语句.
-SELECT * FROM peoples where school IS NOT ‘GDUFS’;
+SELECT * FROM peoples where school not in ‘GDUFS’ or school is null;
 
-#3-2.查找计算机系每次考试所有学生的平均成绩(最终显示考试名称, 平均分)。
+#3-2.查找计算机系每次考试所有学生的平均成绩(最终显示考ho名称, 平均分)。
 SELECT exam.exam_name,avg(exam.grade) FROM exam WHERE student.dept_name = 'computer';
 
 #3-3.查找女学霸（考试平均分达到80分或80分以上的女生的姓名, 分数）。
@@ -23,8 +23,8 @@ update department set dept_name = avg_budget,building = NULL,budget = avg(budget
 Delete from student,exam where student.dept_name=’computer’,avg(exam.grade)<70;
 
 #3-9.找出所有正在谈恋爱,但是学习成绩不佳(考试平均分低于75)的学生,强制将其情感状态改为单身.
-update student set student.emotion_state=’single’ where student.emotion_state=’loving’,avg(exam.grade<75);
-
+update student set student.emotion_state=’single’ where student.emotion_state=’loving’,ID in (select student_ID from exam group by student_ID having avg(grade)<70);
+ 
 #3-10(选做). 对每个学生, 往exam表格中插入名为Avg_Exam的考试, 考试分数为之前学生参加过考试的平均分.
 alter table exam add  column Avg_Exam varchar(20);
 for x in range(count(student.name)):
